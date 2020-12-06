cdde2ef166c70d1aea5bb7bc167010dfd2558ce3
greatbody <sunruicode@gmail.com>
2020-12-06 11:05:59 +0800

    Initial commit

---
+++ WechatMediaRenamer/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace WechatMediaRenamer
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private static readonly DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        private static readonly TimeSpan chinaTimeSpan = new TimeSpan(8, 0, 0);
+        private static readonly TimeZoneInfo chinaTime = TimeZoneInfo.CreateCustomTimeZone("CST", chinaTimeSpan, "China Standard SR Time", "SR Time");
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Ellipse_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                RenameFiles(files);
+            }
+        }
+
+        private void RenameFiles(string[] files)
+        {
+            MediaFileRenamer mediaFileRenamer;
+            StringBuilder sb = new StringBuilder();
+            foreach (var filePath in files)
+            {
+                mediaFileRenamer = MediaFileRenamer.FromFilePath(filePath);
+                string renameMessage = mediaFileRenamer.Rename();
+                if (renameMessage.Length > 0)
+                {
+                    sb.AppendLine(renameMessage);
+                }
+            }
+            if (sb.Length > 0)
+            {
+                MessageBox.Show(sb.ToString());
+            }
+        }
+    }
+}
---
+++ WechatMediaRenamer/MediaFileRenamer.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace WechatMediaRenamer
+{
+    class MediaFileRenamer
+    {
+        public string FullFilePath { get; }
+        public string FullDirectoryPath { get; }
+        public string FileNameWithoutExtension { get; }
+        public string Extension { get; }
+
+        private static readonly DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        private static readonly TimeSpan chinaTimeSpan = new TimeSpan(8, 0, 0);
+        private static readonly TimeZoneInfo chinaTime = TimeZoneInfo.CreateCustomTimeZone("CST", chinaTimeSpan, "China Standard SR Time", "SR Time");
+
+        public static MediaFileRenamer FromFilePath(string filePath)
+        {
+            return new MediaFileRenamer(filePath);
+        }
+
+        public MediaFileRenamer(string filePath)
+        {
+            this.FullFilePath = filePath;
+            this.FullDirectoryPath = Path.GetDirectoryName(filePath);
+            this.FileNameWithoutExtension = Path.GetFileNameWithoutExtension(filePath);
+            this.Extension = Path.GetExtension(filePath);
+        }
+
+        public string Rename()
+        {
+            string epochTime = GetDatePart(this.FileNameWithoutExtension);
+            if (epochTime.Equals(String.Empty) || epochTime.Length != 13)
+            {
+                return String.Format("无法识别文件 '{0}'中的Epoch时间！", this.FullFilePath);
+            }
+            string destPath = Path.Combine(this.FullDirectoryPath, ComposeNameFromEpochTime(epochTime));
+            if (File.Exists(destPath))
+            {
+                return String.Format("'{0}' 无法被重命名为 '{1}'，因为目标已存在！", this.FullFilePath, destPath);
+            }
+            File.Move(this.FullFilePath, destPath);
+            return String.Empty;
+        }
+
+        private string GetDatePart(string fileNameWithoutExtention)
+        {
+            string datePattern = @"[^\d]*(\d+)";
+            Match m = Regex.Match(fileNameWithoutExtention, datePattern, RegexOptions.IgnoreCase);
+            if (m.Success && m.Groups.Count > 1)
+                return m.Groups[1].Value;
+            return String.Empty;
+        }
+
+        private string GetFileNameTemplateByExtention(string extName)
+        {
+            switch (extName.ToLower())
+            {
+                case ".mp4":
+                    return "VID_{0}{1}";
+                case ".jpg":
+                case ".jpeg":
+                case ".png":
+                case ".gif":
+                    return "IMG_{0}{1}";
+                default:
+                    return "{0}{1}";
+            }
+        }
+
+        private string ComposeNameFromEpochTime(string epochTime)
+        {
+            string dateTimePart = String.Format("{0:yyyyMMdd_HHmmss}", GetDateTimeInChinaFromEpochTime(epochTime));
+            string fileNameTemplate = GetFileNameTemplateByExtention(this.Extension);
+            return String.Format(fileNameTemplate, dateTimePart, this.Extension);
+        }
+
+        private DateTime GetDateTimeInChinaFromEpochTime(string epochTime)
+        {
+            DateTime utcDateTime = epoch.AddMilliseconds(long.Parse(epochTime));
+            return TimeZoneInfo.ConvertTimeFromUtc(utcDateTime, chinaTime);
+        }
+    }
+}
